1a3559f5ace666af240caf92424c0a861e6e5a89
Andriy Novykov <novykov.andriy@gmail.com>
2024-08-18 21:39:42 -0700

    Added helm installation to k3s script.

--- k3s.sh
+++ k3s.sh
@@ -6,5 +6,7 @@ sudo apt upgrade -y
 sudo apt install curl -y
 
 # install k3s
-
 curl -sfL https://get.k3s.io | sh -
+
+# install helm
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
